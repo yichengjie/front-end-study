f275063eab4dde381f97e9320a397a9c28415c13
yicj <626659321@qq.com>
2019-07-21 22:27:26 +0800

    s

--- 01/index.js
+++ 01/index.js
@@ -1,17 +1,9 @@
 $(function () {
-    //$(document).off('.datepicker.data-api');
-
-
     //初始化评价日期空间
     $('#content-date-container').datepicker({
         format: 'yyyy-mm-dd',
          startDate: '0d'
     });
-
-    // $(".datepicker").datepicker({
-    //     format: 'yyyy-mm-dd',
-    //     startDate: '0d'
-    // });
 
     $('select').selectpicker();
 })
---
+++ 01/page2.js
@@ -0,0 +1,8 @@
+$(function () {
+    //初始化评价日期空间
+    $('#check-date-container').datepicker({
+        format: 'yyyy-mm-dd',
+        startDate: '0d'
+    });
+
+})
