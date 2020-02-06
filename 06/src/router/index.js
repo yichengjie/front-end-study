b34ac005ac9e95c723833079ffcfdea2594ff740
易成杰 <626659321@qq.com>
2020-02-06 21:44:00 +0800

    保存

--- 06/src/router/index.js
+++ 06/src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import QualityEvaluationList from '../views/quality-evaluation/QualityEvaluationList.vue' ;
-import QualityEvaluationDetail from '../views/quality-evaluation/QualityEvaluationDetail.vue' ;
-import RoutineExaminationList from '../views/routine-examination/RoutineExaminationList.vue' ;
-import RoutineExaminationDetail from '../views/routine-examination/RoutineExaminationDetail.vue' ;
-import ElectronicClassCard from '../views/electronic-class-card/ElectronicClassCard.vue' ;
+//import QualityEvaluationList from '../views/quality-evaluation/QualityEvaluationList.vue' ;
+//import QualityEvaluationDetail from '../views/quality-evaluation/QualityEvaluationDetail.vue' ;
+//import RoutineExaminationList from '../views/routine-examination/RoutineExaminationList.vue' ;
+//import RoutineExaminationDetail from '../views/routine-examination/RoutineExaminationDetail.vue' ;
+//import ElectronicClassCard from '../views/electronic-class-card/ElectronicClassCard.vue' ;
 
 
 Vue.use(VueRouter)
@@ -19,29 +19,32 @@ const routes = [
   {
     path: '/qualityEvaluationList/:teacherNumber/:campusNumber',
     name: 'qualityEvaluationList',
-    component: QualityEvaluationList
-    //component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    //component: QualityEvaluationList
+    component: () => import('../views/quality-evaluation/QualityEvaluationList.vue')
   },
   {
     path: '/qualityEvaluationDetail/:teacherNumber/:campusNumber',
     name: 'qualityEvaluationDetail',
-    component: QualityEvaluationDetail
-    //component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    //component: QualityEvaluationDetail
+    component: () => import('../views/quality-evaluation/QualityEvaluationDetail.vue')
   },
   {
     path: '/routineExaminationList/:teacherNumber/:campusNumber',
     name: 'routineExaminationList',
-    component: RoutineExaminationList
+    //component: RoutineExaminationList
+    component: () => import('../views/routine-examination/RoutineExaminationList.vue')
   },
   {
     path: '/routineExaminationDetail/:teacherNumber/:campusNumber',
     name: 'routineExaminationDetail',
-    component: RoutineExaminationDetail
+    //component: RoutineExaminationDetail
+    component:() => import('../views/routine-examination/RoutineExaminationDetail.vue')
   },
   {
     path: '/electronicClassCard/:teacherNumber/:campusNumber',
     name: 'electronicClassCard',
-    component: ElectronicClassCard
+    //component: ElectronicClassCard
+    component:() => import('../views/electronic-class-card/ElectronicClassCard.vue')
   },
 ]
 
